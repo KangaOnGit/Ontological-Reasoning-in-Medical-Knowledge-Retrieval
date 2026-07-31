51a3fc53529bb9d5367434fe2dc82fa9121a670e
Khang (Kangaroo) <khangdo897@gmail.com>
2026-07-31 23:21:13 +0700

    minor changes

--- app.py
+++ app.py
@@ -95,7 +95,6 @@ async def predict(
 
 if __name__ == "__main__":
     import uvicorn
-
     uvicorn.run("app:app", host="0.0.0.0", port=8000, reload=False)
 
 # uvicorn app:app --host 0.0.0.0 --port 8000
